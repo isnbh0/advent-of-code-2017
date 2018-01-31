c724467adf0d8af23c50ab45bfb581540a60a204
isnbh0 <isnbh0@gmail.com>
2018-01-31 17:24:25 +0900

    edit python scripts to follow __main__ convention

--- day01/part1.py
+++ day01/part1.py
@@ -2,13 +2,18 @@ import sys
 sys.path.append('..')
 from get_input import get_input_by_lines
 
-def get_repeat_sum(num):
-    numlist = str(num) + str(num)[0]
-    sum_ = 0
-    for idx, digit in enumerate(numlist[:-1]):
-        sum_ += int(digit == numlist[idx+1])*int(digit)
-    return sum_
 
-ex1 = ','.join(get_input_by_lines(1))
-print(get_repeat_sum(ex1))
+def main():
+    def get_repeat_sum(num):
+        numlist = str(num) + str(num)[0]
+        sum_ = 0
+        for idx, digit in enumerate(numlist[:-1]):
+            sum_ += int(digit == numlist[idx+1])*int(digit)
+        return sum_
+
+    ex1 = ','.join(get_input_by_lines(1))
+    print(get_repeat_sum(ex1))
+
+if __name__ == '__main__':
+    main()
 
--- day03/part2.py
+++ day03/part2.py
@@ -4,81 +4,86 @@ from get_input import get_input_by_lines
 
 import numpy as np
 
-corner_map = {
-    0 : ( 1,  1),
-    1 : (-1,  1),
-    2 : (-1, -1),
-    3 : ( 1, -1)
-}
 
-minus_map = {
-    0 : ( 0, -1),
-    1 : ( 1,  0),
-    2 : ( 0,  1),
-    3 : (-1,  0)
-}
+def main():
+    corner_map = {
+        0 : ( 1,  1),
+        1 : (-1,  1),
+        2 : (-1, -1),
+        3 : ( 1, -1)
+    }
 
-def odd_sqrt(x):
-    if x == 1:
-        return 0
-    n = int((x-1)**0.5)
-    return n if n&1 else (n-1)
+    minus_map = {
+        0 : ( 0, -1),
+        1 : ( 1,  0),
+        2 : ( 0,  1),
+        3 : (-1,  0)
+    }
 
-def find_corner(x):
-    if x == 1:
-        return 0, 0
-    s = odd_sqrt(x)
-    return ((x - s**2 - 1) // (s+1)), odd_sqrt(x)
+    def odd_sqrt(x):
+        if x == 1:
+            return 0
+        n = int((x-1)**0.5)
+        return n if n&1 else (n-1)
 
-def get_coord(x):
-    if x == 1:
-        return (0, 0)
-    corner_type, start_sq = find_corner(x)
-    upper_right = ((start_sq+1)/2, (start_sq+1)/2)
-    
-    coord = np.array(upper_right) * np.array(corner_map[corner_type])
-    coord += (start_sq**2 + (corner_type+1)*(start_sq+1) - x)\
-            *(np.array(minus_map[corner_type]))
-    return tuple([int(x) for x in coord])
+    def find_corner(x):
+        if x == 1:
+            return 0, 0
+        s = odd_sqrt(x)
+        return ((x - s**2 - 1) // (s+1)), odd_sqrt(x)
 
-
-class SpiralGrid():
-    def __init__(self, rule_func):
-        self.grid = dict()
-        self.rule = rule_func
-        self.grid[(0, 0)] = 1
-        return
-    
-    def step(self):
-        grid_size = len(self.grid)
-        new_coord = get_coord(grid_size+1)
+    def get_coord(x):
+        if x == 1:
+            return (0, 0)
+        corner_type, start_sq = find_corner(x)
+        upper_right = ((start_sq+1)/2, (start_sq+1)/2)
         
-        self.grid[new_coord]\
-        = self.rule(self.grid, new_coord)
+        coord = np.array(upper_right) * np.array(corner_map[corner_type])
+        coord += (start_sq**2 + (corner_type+1)*(start_sq+1) - x)\
+                *(np.array(minus_map[corner_type]))
+        return tuple([int(x) for x in coord])
+
+
+    class SpiralGrid():
+        def __init__(self, rule_func):
+            self.grid = dict()
+            self.rule = rule_func
+            self.grid[(0, 0)] = 1
+            return
         
-        return self.grid[new_coord]
+        def step(self):
+            grid_size = len(self.grid)
+            new_coord = get_coord(grid_size+1)
+            
+            self.grid[new_coord]\
+            = self.rule(self.grid, new_coord)
+            
+            return self.grid[new_coord]
 
-def rule(grid, new_coord):
-    new_coord = np.array(new_coord)
-    total = 0
-    for i in [-1, 0, 1]:
-        for j in [-1, 0, 1]:
-            try:
-                total += grid[tuple(new_coord + np.array((i, j)))]
-            except:
-                pass
-    return total
+    def rule(grid, new_coord):
+        new_coord = np.array(new_coord)
+        total = 0
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                try:
+                    total += grid[tuple(new_coord + np.array((i, j)))]
+                except:
+                    pass
+        return total
 
 
-def part2(input_, rule):
-    grid = SpiralGrid(rule)
-    x = 0
-    while x <= input_:
-        x = grid.step()
-    return x
+    def part2(input_, rule):
+        grid = SpiralGrid(rule)
+        x = 0
+        while x <= input_:
+            x = grid.step()
+        return x
 
 
-ex3 = int(get_input_by_lines(3)[0])
+    ex3 = int(get_input_by_lines(3)[0])
 
-print(part2(ex3, rule))
+    print(part2(ex3, rule))
+
+if __name__ == '__main__':
+    main()
 
