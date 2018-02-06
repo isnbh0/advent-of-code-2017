6dbe2b2a356fd09002e74d167e213444e4bde7d6
isnbh0 <isnbh0@gmail.com>
2018-02-06 16:04:35 +0900

    fix day24 solutions

--- day24/part2.py
+++ day24/part2.py
@@ -3,19 +3,22 @@ sys.path.append('..')
 from get_input import get_input_by_lines
 
 import numpy as np
-from classes import VirusGrid
+from functions import make_bridges
 
 
 def main():
-    print("This code takes about 55s to run. Ctrl+C to break before then.")
+    print("This code takes about 6min to run. Ctrl+C to break before then.")
 
-    ex22_0 = get_input_by_lines(22)
-    ex22 = np.array([list(x) for x in ex22_0]) 
+    ex24_0 = get_input_by_lines(24)
+    ex24 = [tuple(map(int, x.split('/'))) for x in ex24_0]
 
-    virusgrid = VirusGrid(ex22)
-    for i in range(10000000):
-        virusgrid.burstv2()
-    print(virusgrid.infect_cnt)
+    bridges24 = make_bridges(ex24)
+
+    len24 = [len(x) for x in bridges24]
+    max_length = max(len24)
+
+    long24 = [x for x in bridges24 if len(x) == max_length]
+    print(max([np.array(x).flatten().sum() for x in long24]))
 
 if __name__ == '__main__':
     main()
